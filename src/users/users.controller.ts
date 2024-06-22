6aa76dcbaa45efb788353e36294b208f558d32ee
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-06-22 17:04:37 +0500

    patch,delete,post routes added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,38 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get()
-  getAllUsers(): string[] {
+  @Get() // GET /users or /users?role=value
+  getAllUsers(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'): string[] {
     return ['ali', 'awais', 'hassan'];
   }
 
-  @Get(':id')
+  @Get(':id') // GET /users/:id
   getOneUser(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  createUser(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  updateUser(@Param('id') id: string, @Body() updatedUser: {}) {
+    return { id, ...updatedUser };
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  deleteUser(@Param('id') id: string) {
     return { id };
   }
 }
